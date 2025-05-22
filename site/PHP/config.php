6e549efecf40d1d042e5028d065e9d631b0a8e17
Julia2100 <0001062238@senimgaluno.com.br>
2025-05-22 19:22:12 -0300

    devolta

--- site/PHP/config.php
+++ site/PHP/config.php
@@ -1,11 +1,19 @@
 <?php
-$access_token = "APP_USR-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+// Exibe erros na tela (para desenvolvimento)
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Configuração do banco de dados
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "pousada_ypua";
+$dbname = "sousada";
 
+// Cria a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verifica conexão
 if ($conn->connect_error) {
-    die("Erro ao conectar no banco de dados: " . $conn->connect_error);
+    die("<h2>Erro ao conectar no banco de dados:</h2><p>" . $conn->connect_error . "</p>");
 }
+?>
